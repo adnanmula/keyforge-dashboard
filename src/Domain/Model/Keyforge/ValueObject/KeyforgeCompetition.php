b93e05b740c2965f7c48509cf3b1d5a8e7ff0eb4
adnan <adnan97m@gmail.com>
2023-01-18 03:37:13 +0100

    misc improvements

--- src/Domain/Model/Keyforge/ValueObject/KeyforgeCompetition.php
+++ src/Domain/Model/Keyforge/ValueObject/KeyforgeCompetition.php
@@ -8,8 +8,7 @@ enum KeyforgeCompetition: string
     case TCO_CASUAL = 'TCO Casual';
     case TCO_COMPETITIVE = 'TCO Competitive';
     case NKFL_LEAGUE_SEASON_19 = 'NKFL League Season 19';
-    case NKFL_LEAGUE_CUP_SEASON_19 = 'NKFL League Cup Season 19';
-    case NKFL_LEAGUE_CUP_REVERSAL_SEASON_19 = 'NKFL League Cup Reversal Season 19';
+    case NKFL_LEAGUE_CUP_SEASON_19 = 'NKFL League Cups Season 19';
     case NKFL_ARCHON_TOURNAMENT = 'NKFL Archon Tournament';
     case LOCAL_LEAGUE = 'Local League';
     case LOCAL_CUP = 'Local Cup';
@@ -22,7 +21,6 @@ enum KeyforgeCompetition: string
             self::TCO_COMPETITIVE->name => self::TCO_COMPETITIVE,
             self::NKFL_LEAGUE_SEASON_19->name => self::NKFL_LEAGUE_SEASON_19,
             self::NKFL_LEAGUE_CUP_SEASON_19->name => self::NKFL_LEAGUE_CUP_SEASON_19,
-            self::NKFL_LEAGUE_CUP_REVERSAL_SEASON_19->name => self::NKFL_LEAGUE_CUP_REVERSAL_SEASON_19,
             self::NKFL_ARCHON_TOURNAMENT->name => self::NKFL_ARCHON_TOURNAMENT,
             self::LOCAL_LEAGUE->name => self::LOCAL_LEAGUE,
             self::LOCAL_CUP->name => self::LOCAL_CUP,
@@ -38,7 +36,6 @@ enum KeyforgeCompetition: string
             self::TCO_COMPETITIVE->name,
             self::NKFL_LEAGUE_SEASON_19->name,
             self::NKFL_LEAGUE_CUP_SEASON_19->name,
-            self::NKFL_LEAGUE_CUP_REVERSAL_SEASON_19->name,
             self::NKFL_ARCHON_TOURNAMENT->name,
             self::LOCAL_LEAGUE->name,
             self::LOCAL_CUP->name,
