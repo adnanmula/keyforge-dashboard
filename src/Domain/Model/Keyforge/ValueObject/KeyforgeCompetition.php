2f5ba9738d5dc0c22aeb63042b716d974879714c
Adnan Mula <adnanalfonso.mula@pccomponentes.com>
2023-10-11 14:55:40 +0200

    add competitions

--- src/Domain/Model/Keyforge/ValueObject/KeyforgeCompetition.php
+++ src/Domain/Model/Keyforge/ValueObject/KeyforgeCompetition.php
@@ -14,11 +14,13 @@ enum KeyforgeCompetition: string
     case NKFL_LEAGUE_SEASON_19 = 'NKFL League Season 19';
     case NKFL_LEAGUE_SEASON_20 = 'NKFL League Season 20';
     case NKFL_LEAGUE_SEASON_21 = 'NKFL League Season 21';
+    case NKFL_LEAGUE_SEASON_22 = 'NKFL League Season 22';
     case NKFL_LEAGUE_CUP_SEASON_19 = 'NKFL League Cups Season 19';
     case NKFL_LEAGUE_CUP_SEASON_21 = 'NKFL League Cups Season 21';
     case NKFL_ARCHON_TOURNAMENT = 'NKFL Archon Tournament';
     case LOCAL_LEAGUE = 'Local League';
     case LOCAL_CUP = 'Local Cup';
+    case VT23_FIRENZE = 'VT23 FIRENZE';
 
     public static function fromName(string $name): self
     {
@@ -29,11 +31,13 @@ enum KeyforgeCompetition: string
             self::NKFL_LEAGUE_SEASON_19->name => self::NKFL_LEAGUE_SEASON_19,
             self::NKFL_LEAGUE_SEASON_20->name => self::NKFL_LEAGUE_SEASON_20,
             self::NKFL_LEAGUE_SEASON_21->name => self::NKFL_LEAGUE_SEASON_21,
+            self::NKFL_LEAGUE_SEASON_22->name => self::NKFL_LEAGUE_SEASON_22,
             self::NKFL_LEAGUE_CUP_SEASON_19->name => self::NKFL_LEAGUE_CUP_SEASON_19,
             self::NKFL_LEAGUE_CUP_SEASON_21->name => self::NKFL_LEAGUE_CUP_SEASON_21,
             self::NKFL_ARCHON_TOURNAMENT->name => self::NKFL_ARCHON_TOURNAMENT,
             self::LOCAL_LEAGUE->name => self::LOCAL_LEAGUE,
             self::LOCAL_CUP->name => self::LOCAL_CUP,
+            self::VT23_FIRENZE->name => self::VT23_FIRENZE,
             default => self::FRIENDS,
         };
     }
