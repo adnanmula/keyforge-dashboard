1f58f9f64e4bfbcc5739bfc8c4a5b9a62cfa6829
adnanmula <adnan97m@gmail.com>
2025-04-08 21:18:20 +0200

    default tag on account approval

--- src/Application/Command/Shared/User/ApproveAccount/ApproveAccountCommandHandler.php
+++ src/Application/Command/Shared/User/ApproveAccount/ApproveAccountCommandHandler.php
@@ -2,11 +2,19 @@
 
 namespace AdnanMula\Cards\Application\Command\Shared\User\ApproveAccount;
 
+use AdnanMula\Cards\Domain\Model\Keyforge\Deck\KeyforgeDeckTag;
+use AdnanMula\Cards\Domain\Model\Keyforge\Deck\KeyforgeTagRepository;
 use AdnanMula\Cards\Domain\Model\Keyforge\User\KeyforgeUser;
 use AdnanMula\Cards\Domain\Model\Keyforge\User\KeyforgeUserRepository;
 use AdnanMula\Cards\Domain\Model\Shared\Exception\UserNotExistsException;
 use AdnanMula\Cards\Domain\Model\Shared\UserRepository;
+use AdnanMula\Cards\Domain\Model\Shared\ValueObject\Locale;
+use AdnanMula\Cards\Domain\Model\Shared\ValueObject\TagStyle;
+use AdnanMula\Cards\Domain\Model\Shared\ValueObject\TagType;
+use AdnanMula\Cards\Domain\Model\Shared\ValueObject\TagVisibility;
 use AdnanMula\Cards\Domain\Model\Shared\ValueObject\UserRole;
+use AdnanMula\Cards\Domain\Model\Shared\ValueObject\Uuid;
+use AdnanMula\Cards\Shared\LocalizedString;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 
 final readonly class ApproveAccountCommandHandler
@@ -14,6 +22,7 @@ final readonly class ApproveAccountCommandHandler
     public function __construct(
         private UserRepository $repository,
         private KeyforgeUserRepository $kfUserRepository,
+        private KeyforgeTagRepository $kfTagsRepository,
     ) {}
 
     public function __invoke(ApproveAccountCommand $command): void
@@ -32,6 +41,25 @@ final readonly class ApproveAccountCommandHandler
             $user->setRole(UserRole::ROLE_KEYFORGE);
 
             $this->kfUserRepository->save(KeyforgeUser::create($user->id(), $user->name(), null));
+
+            $this->kfTagsRepository->save(new KeyforgeDeckTag(
+                Uuid::v4(),
+                LocalizedString::fromArray(
+                    [
+                        Locale::es_ES->value => 'Fav',
+                        Locale::en_GB->value => 'Fav',
+                    ],
+                ),
+                TagVisibility::PRIVATE,
+                TagStyle::from([
+                    TagStyle::COLOR_BG => '#e8db1e',
+                    TagStyle::COLOR_TEXT => '#000000',
+                    TagStyle::COLOR_OUTLINE => '#e8db1e',
+                ]),
+                TagType::CUSTOM,
+                false,
+                $user->id(),
+            ));
         } else {
             $user->setRole(UserRole::ROLE_REJECTED_ACCOUNT);
         }
