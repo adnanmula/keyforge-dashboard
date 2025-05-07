2afcc1d58609245bf5ab253c2ccc24916cf1c55d
adnanmula <adnan97m@gmail.com>
2025-05-07 22:15:03 +0200

    update classification

--- src/Application/Query/Keyforge/Competition/GetCompetitionDetailQueryHandler.php
+++ src/Application/Query/Keyforge/Competition/GetCompetitionDetailQueryHandler.php
@@ -4,6 +4,8 @@ namespace AdnanMula\Cards\Application\Query\Keyforge\Competition;
 
 use AdnanMula\Cards\Domain\Model\Keyforge\Game\KeyforgeCompetition;
 use AdnanMula\Cards\Domain\Model\Keyforge\Game\KeyforgeCompetitionRepository;
+use AdnanMula\Cards\Domain\Model\Keyforge\Game\KeyforgeGame;
+use AdnanMula\Cards\Domain\Model\Keyforge\Game\KeyforgeGameRepository;
 use AdnanMula\Cards\Domain\Model\Shared\ValueObject\Uuid;
 use AdnanMula\Criteria\Criteria;
 use AdnanMula\Criteria\Filter\Filter;
@@ -11,6 +13,7 @@ use AdnanMula\Criteria\Filter\FilterType;
 use AdnanMula\Criteria\FilterField\FilterField;
 use AdnanMula\Criteria\FilterGroup\AndFilterGroup;
 use AdnanMula\Criteria\FilterValue\FilterOperator;
+use AdnanMula\Criteria\FilterValue\StringArrayFilterValue;
 use AdnanMula\Criteria\FilterValue\StringFilterValue;
 use AdnanMula\Tournament\Classification\Player;
 
@@ -18,6 +21,7 @@ final readonly class GetCompetitionDetailQueryHandler
 {
     public function __construct(
         private KeyforgeCompetitionRepository $repository,
+        private KeyforgeGameRepository $gameRepository,
     ) {}
 
     public function __invoke(GetCompetitionDetailQuery $query): KeyforgeCompetition
@@ -32,11 +36,55 @@ final readonly class GetCompetitionDetailQueryHandler
             ));
         }
 
+        $games = [];
+
         if (null !== $competition->startedAt) {
+            $gameIds = [];
+
             foreach ($fixtures as $fixture) {
                 $competition->fixtures->add($fixture);
+                $gameIds = array_merge($gameIds, $fixture->games);
+            }
+
+            if (count($gameIds) > 0) {
+                $games = $this->games(...$gameIds);
+            }
+
+
+            foreach ($fixtures as $fixture) {
+                if (null === $fixture->winner) {
+                    continue;
+                }
+
+                $winner = $competition->classification->playerWithId($fixture->winner->value());
+                $winner->addWin();
+                $loser = null;
+
+                foreach ($fixture->games as $gameId) {
+                    $game = $games[$gameId->value()];
+
+                    if (null === $game) {
+                        continue;
+                    }
+
+                    $winner->addGameWin();
+                    $winner->addPointsPositive(3);
+                    $winner->addPointsNegative($game->score()->loserScore());
+
+                    $loser = $competition->classification->playerWithId($game->loser()->value());
+
+                    if (null !== $loser) {
+                        $loser->addGameLoss();
+                        $loser->addPointsPositive($game->score()->loserScore());
+                        $loser->addPointsNegative(3);
+                    }
+                }
+
+                $loser?->addLose();
             }
         }
+
+        $competition->classification->order();
 
         return $competition;
     }
@@ -64,5 +112,31 @@ final readonly class GetCompetitionDetailQueryHandler
         }
 
         return $competition;
+    }
+
+    /** @return array<string, KeyforgeGame> */
+    private function games(Uuid ...$ids): array
+    {
+        $games = $this->gameRepository->search(new Criteria(
+            null,
+            null,
+            null,
+            new AndFilterGroup(
+                FilterType::AND,
+                new Filter(
+                    new FilterField('id'),
+                    new StringArrayFilterValue(...\array_map(static fn ($id) => $id->value(), $ids)),
+                    FilterOperator::IN,
+                ),
+            ),
+        ));
+
+        $indexedGames = [];
+
+        foreach ($games as $game) {
+            $indexedGames[$game->id()->value()] = $game;
+        }
+
+        return $indexedGames;
     }
 }
